ca776c0b6a9343c55d55b06da2df27ec92c4f2e9
pro-code-designer <prodesigner7979@gmail.com>
2025-01-24 11:36:10 +0330

    last

---
+++ Assets/LevelData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    [SerializeField] private bool ToiletLevel;
+    [SerializeField] private bool BubbleLevel;
+    [SerializeField] private bool DuckHaunt;
+}
--- Assets/Mohammad/Code/BoxHandler.cs
+++ Assets/Mohammad/Code/BoxHandler.cs
@@ -4,9 +4,17 @@ namespace Mohammad.Code
 {
     public class BoxHandler : MonoBehaviour
     {
+        [SerializeField] private GameObject Box;
         public void OnMouseDown()
         {
-            
+            if (Box != null) 
+                Box.SetActive(true);
+        }
+
+        public void buxFalse()
+        {
+            if (Box != null)
+                Box.SetActive(false);
         }
     }
 }
